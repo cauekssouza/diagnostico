1061c6d5a2cbbf48fe0e54cc541fcd8fc77a1a33
Cauê Klosoviski Stadler de Souza <107008671+cauekssouza@users.noreply.github.com>
2024-08-03 11:43:42 -0300

    Update Simples.java

--- Diagnostico/Simples.java
+++ Diagnostico/Simples.java
@@ -1,7 +1,7 @@
 public class Simples implements  Lavar{
     @Override
     public void lavar(Carro carro){
-        System.out.println("\nLavagem simples iniciada para o carro: " + carro.getModelo() + "\ne de\n" + carro.getCor());
+        System.out.println("\nLavagem simples iniciada para o carro: " + carro.getModelo() + "\ne de cor\n" + carro.getCor());
         System.out.println("\nCarro lavado Com Sucesso.");
     }
 }
