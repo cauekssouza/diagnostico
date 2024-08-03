3533439760786b667af37eb785ace7a634087be3
Cauê Klosoviski Stadler de Souza <107008671+cauekssouza@users.noreply.github.com>
2024-08-03 11:39:18 -0300

    Update Premium.java

--- Diagnostico/Premium.java
+++ Diagnostico/Premium.java
@@ -2,6 +2,6 @@ public class Premium implements Lavar{
     @Override
     public void lavar(Carro carro){
         System.out.println("\nLavagem premium \niniciada para o carro: " + carro.getModelo());
-        System.out.println("\nCarro lavado externamente, \ninternamente e encerado.");
+        System.out.println("nCarro lavado com sucesso, obrigado pela compressão");
     }
 }
